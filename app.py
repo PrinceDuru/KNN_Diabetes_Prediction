09e97c285adb93578440df611ccf3d1a6a7e3a05
PrinceDuru <prince.i.duru@gmail.com>
2022-01-15 18:16:28 -0500

    added Procfile for Heroku deployment

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request, render_template
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 import pickle
 
 app = Flask(__name__)
 
+# Load the Model
 model = pickle.load(open('model.pkl', 'rb'))
-# scaler = StandardScaler()
+
+# Import scaler object used to fit the training data
 from model_train import scaler
 
 @app.route('/')
+@app.route('/home')
+@app.route('/predict')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', Home='HOME')
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -19,16 +22,15 @@ def predict():
     For rendering results on HTML GUI
     '''
     float_features = [float(x) for x in request.form.values()]
-    final_features = scaler.transform(np.array(float_features).reshape(1, -1)) #[np.array(int_features)]
+    final_features = scaler.transform(np.array(float_features).reshape(1, -1))
     prediction = model.predict(final_features)
-
-    # output = round(prediction[0], 2)
+   
     if prediction:
-        return render_template('index.html', prediction_text='Patient is predicted to have Diabetes')
+        return render_template('index.html', positive_prediction_text='Patient is predicted to have Diabetes')
     else:
-        return render_template('index.html', prediction_text='Patient is predicted to NOT have Diabetes')
+        return render_template('index.html', negative_prediction_text='Patient is predicted to NOT have Diabetes')
         
-    # return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+   
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
